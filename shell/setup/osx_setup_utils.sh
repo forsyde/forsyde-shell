5e463f75558b92d980d582e884399cc7ec1737be
José Edil Guimarães De Medeiros <j.edil@ene.unb.br>
2017-01-11 17:13:10 +0100

    Check if iTerm is installed or use Terminal.app instead

--- shell/setup/osx_setup_utils.sh
+++ shell/setup/osx_setup_utils.sh
@@ -32,8 +32,16 @@ function update_package_manager () {
 
 function create_runner () {
     touch forsyde-shell
-    echo '#!/bin/bash
-open -a Terminal.app shell/osx_runner' > forsyde-shell
+    echo '#!/bin/bash' > forsyde-shell
+
+    iterm_var=`mdfind 'kMDItemKind=="Application" && kMDItemDisplayName="iTerm"'`
+    if [ -n "$iterm_var" ]
+    then
+        echo 'open -a iTerm -n shell/osx_runner' >> forsyde-shell
+    else
+        echo 'open -a Terminal.app shell/osx_runner' >> forsyde-shell
+    fi
+
     chmod +x forsyde-shell
 
     touch shell/osx_runner
